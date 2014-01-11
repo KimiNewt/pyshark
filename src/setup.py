992cb1914d3699284f8f8a06ff4d8690efc44126
KimiNewt <dorgreen1@gmail.com>
2014-01-11 08:17:04 +0200

    Updated setup with more details.

--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('../README.txt') as f:
+    long_description = f.read()
+
 setup(
     name = "pyshark",
     version = "0.1",
@@ -7,8 +10,10 @@ setup(
 
     install_requires = ['lxml', 'py'],
 
+    url = "https://github.com/KimiNewt/pyshark",
+    long_description=long_description,
     author = "Dor Green",
     author_email = "dorgreen1 at gmail dot com",
-    description = "",
-    keywords = "wireshark capture packets",
+    description = "Python wrapper for tshark, allowing python packet parsing using wireshark dissectors",
+    keywords = "wireshark capture packets parsing packet",
 )

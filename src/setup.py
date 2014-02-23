2aecd6659da8ecd375df029a21450f13a39cf3e3
KimiNewt <dorgreen1@gmail.com>
2014-02-23 13:30:58 +0200

    Updated setup version

--- src/setup.py
+++ src/setup.py
@@ -5,7 +5,7 @@ with open('README.txt') as f:
 
 setup(
     name = "pyshark",
-    version = "0.2.1",
+    version = "0.2.2",
     packages = find_packages(),
 
     install_requires = ['lxml', 'py'],

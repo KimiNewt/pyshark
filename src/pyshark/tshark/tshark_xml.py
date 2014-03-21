7de7f357aa17afdb5c7f4383169f1924cf89d3cd
palaviv <palaviv@gmail.com>
2014-03-21 16:20:02 +0200

    Changed tshark_xml to work with field instead of Layer

--- src/pyshark/tshark/tshark_xml.py
+++ src/pyshark/tshark/tshark_xml.py
@@ -2,7 +2,7 @@
 This module contains functions to turn TShark XML parts into Packet objects.
 """
 import lxml.objectify
-from pyshark.packet.layer import Layer
+from pyshark.packet.field import Field
 from pyshark.packet.packet import Packet
 
 
@@ -15,11 +15,11 @@ def packet_from_xml_packet(xml_pkt):
     """
     if not isinstance(xml_pkt, lxml.objectify.ObjectifiedElement):
         xml_pkt = lxml.objectify.fromstring(xml_pkt)
-    layers = [Layer(proto) for proto in xml_pkt.proto]
+    layers = [Field(proto) for proto in xml_pkt.proto]
     geninfo, frame, layers = layers[0], layers[1], layers[2:]
     frame.raw_mode = True
-    return Packet(layers=layers, length=geninfo.get_field_value('len'), sniff_time=geninfo.get_field_value('timestamp', raw=True),
-                  captured_length=geninfo.get_field_value('caplen'), interface_captured=frame.get_field_value('interface_id'))
+    return Packet(layers=layers, length=geninfo['len'].get_Attr('value'), sniff_time=geninfo['timestamp'].get_Attr('value'),
+                  captured_length=geninfo['caplen'].get_Attr('value'), interface_captured=None)
 
 
 def packets_from_xml(xml_data):

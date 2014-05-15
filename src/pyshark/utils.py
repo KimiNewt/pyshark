acf45cdb1fd8bd606b0923b8aa9c1a1970fb75b5
Jeff Ito <bitbucket@delnoch.net>
2014-05-14 21:42:31 -0400

    Fix 'invalid thread id' issue #13 by making sure tid is a C long int.

--- src/pyshark/utils.py
+++ src/pyshark/utils.py
@@ -22,7 +22,7 @@ def _async_raise(tid, exctype):
     """
     if not inspect.isclass(exctype):
         raise TypeError("Only types can be raised (not instances)")
-    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(tid,
+    res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(tid),
                                                   ctypes.py_object(exctype))
     if res == 0:
         raise ValueError("invalid thread id")

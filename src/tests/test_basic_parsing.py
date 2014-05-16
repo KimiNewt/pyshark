03271162e6933c2f541a18548986dd7ea2d542c6
KimiNewt <dorgreen1@gmail.com>
2014-05-16 12:03:34 +0300

    Changed tests from UnitTest to py.test

---
+++ src/tests/test_basic_parsing.py
@@ -0,0 +1,61 @@
+import os
+import pytest
+
+import pyshark
+
+@pytest.fixture
+def caps_directory():
+    return os.path.join(os.path.dirname(__file__), 'caps')
+
+@pytest.fixture
+def simple_capture(request, caps_directory):
+    cap_path = os.path.join(caps_directory, 'capture_test.pcapng')
+    cap = pyshark.FileCapture(cap_path, lazy=False)
+
+    def finalizer():
+        cap.close()
+    request.addfinalizer(finalizer)
+    return cap
+
+
+def test_count_packets(simple_capture):
+    """Test to make sure the right number of packets are read from a known
+       capture"""
+    packet_count = sum(1 for packet in simple_capture)
+    assert packet_count == 24
+
+
+def test_sum_lengths(simple_capture):
+    """Test to make sure that the right packet length is being read from
+       tshark's output by comparing the aggregate length of all packets
+       to a known value"""
+    total_length = sum(int(packet.length) for packet in simple_capture)
+    assert total_length == 2178
+
+
+def test_layers(simple_capture):
+    """Test to make sure the correct protocols are reported for known
+       packets"""
+    packet_indexes = (0, 5, 6, 13, 14, 17, 23)
+    test_values = [simple_capture[i].highest_layer for i in packet_indexes]
+    known_values = ['DNS', 'DNS', 'ICMP', 'ICMP', 'TCP', 'HTTP', 'TCP']
+    assert test_values == known_values
+
+
+def test_ethernet(simple_capture):
+    """Test to make sure Ethernet fields are being read properly by comparing
+       packet dissection results to known values"""
+    packet = simple_capture[0]
+    test_values = packet.eth.addr, packet.eth.dst
+    known_values = ('00:00:bb:10:20:10', '00:00:bb:02:04:01')
+    assert test_values == known_values
+
+def test_icmp(simple_capture):
+    """Test to make sure ICMP fields are being read properly by comparing
+       packet dissection results to known values"""
+    packet = simple_capture[11]
+    # The value returned by tshark is locale-dependent.
+    # Depending on the locale, a comma can be used instead of a dot
+    # as decimal separator.
+    resptime = packet.icmp.resptime.replace(',', '.')
+    assert resptime == '1.667'

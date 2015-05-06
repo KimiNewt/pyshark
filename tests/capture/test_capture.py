c654bc1fdacdb355b7e03c853ebcdc919ac5f91d
Dor Green <dor.green@lightcyber.com>
2015-05-06 11:54:55 +0300

    Fix tests to avoid dict ordering problem

--- tests/capture/test_capture.py
+++ tests/capture/test_capture.py
@@ -12,6 +12,8 @@ def test_capture_gets_multiple_decoding_parameters():
     c = Capture(decode_as={'tcp.port==8888': 'http', 'tcp.port==6666': 'dns'})
     params = c.get_parameters()
     decode_index = params.index('-d')
-    assert params[decode_index + 1] == 'tcp.port==8888,http'
+    possible_results = ['tcp.port==8888,http', 'tcp.port==6666,dns']
+    assert params[decode_index + 1] in possible_results  
+    possible_results.remove(params[decode_index + 1])
     decode_index = params.index('-d', decode_index + 1)
-    assert params[decode_index + 1] == 'tcp.port==6666,dns'
+    assert params[decode_index + 1] == possible_results[0] 

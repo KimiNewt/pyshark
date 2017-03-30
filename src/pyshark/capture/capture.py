90b6dab536284427248c582853ffbf7880c62710
Rémy Léone <remy.leone@gmail.com>
2017-03-30 13:40:33 +0200

    Include raw

--- src/pyshark/capture/capture.py
+++ src/pyshark/capture/capture.py
@@ -43,7 +43,7 @@ class Capture(object):
     def __init__(self, display_filter=None, only_summaries=False, eventloop=None,
                  decryption_key=None, encryption_type='wpa-pwd', output_file=None,
                  decode_as=None,  disable_protocol=None, tshark_path=None,
-                 override_prefs=None, capture_filter=None, use_json=False):
+                 override_prefs=None, capture_filter=None, use_json=False, include_raw=False):
         self._packets = []
         self.current_packet = 0
         self.display_filter = display_filter
@@ -59,6 +59,7 @@ class Capture(object):
         self.override_prefs = override_prefs
         self.debug = False
         self.use_json = use_json
+        self.include_raw = include_raw
 
         self.eventloop = eventloop
         if self.eventloop is None:
@@ -403,6 +404,9 @@ class Capture(object):
             params += ['-f', self.capture_filter]
         if self.display_filter:
             params += [get_tshark_display_filter_flag(self.tshark_path), self.display_filter]
+        # Raw packet hex data can only be printed as text, PostScript, JSON or EK JSON
+        if self.include_raw and self.use_json:
+            params += ["-x"]
         if packet_count:
             params += ['-c', str(packet_count)]
         if all(self.encryption):

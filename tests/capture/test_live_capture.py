b707ef8316a578e374b59c9ad93af4c3d607fd2f
Jonathan GAYVALLET <jonathan.mael.gayvallet@gmail.com>
2017-07-31 13:10:31 +0200

    Seems like travis has some issues with get_tshark_interfaces

--- tests/capture/test_live_capture.py
+++ tests/capture/test_live_capture.py
@@ -1,18 +1,26 @@
+import mock
 import pytest
+
 import pyshark
 
-@pytest.fixture
-def capture():
-    return pyshark.LiveCapture()
 
-@pytest.mark.parametrize("interfaces", [["wlan0"]])
+@pytest.yield_fixture(params=[["wlan0"], ["wlan0mon", "wlan1mon"]])
+def interfaces(request):
+    with mock.patch("pyshark.tshark.tshark.get_tshark_interfaces", return_value=request.param):
+        yield request.param
+
+
+@pytest.fixture
+def capture(interfaces):
+    return pyshark.LiveCapture(interface=interfaces)
+
+
 @pytest.mark.parametrize("monitoring", [True, False])
 def test_get_dumpcap_interface_parameter(capture, monitoring, interfaces):
-    #type: (pyshark.LiveCapture, bool, list) -> None
+    # type: (pyshark.LiveCapture, bool, list) -> None
     capture.monitor_mode = monitoring
-    capture.interfaces = interfaces
     dumpcap_parameters = capture._get_dumpcap_parameters()
-    dumpcap_interfaces = [ dumpcap_parameters[index+1]
-                           for index, value in enumerate(dumpcap_parameters)
-                           if value == "-i" ]
+    dumpcap_interfaces = [dumpcap_parameters[index + 1]
+                          for index, value in enumerate(dumpcap_parameters)
+                          if value == "-i"]
     assert dumpcap_interfaces == interfaces

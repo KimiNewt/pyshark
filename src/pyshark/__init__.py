a2d41e562ff9d56735f6b55e0fd3b6d6a3e68ada
Kevin <068ant@gmail.com>
2019-04-13 21:28:29 +1000

    Added a local gitignore and changed __init__.py to use sys.hexversion as I feel it's more readable

--- src/pyshark/__init__.py
+++ src/pyshark/__init__.py
@@ -4,11 +4,11 @@ import sys
 class UnsupportedVersionException(Exception):
     pass
 
-
-if sys.version_info[0] < 3 or (sys.version_info[0] == 3 and sys.version_info[1] < 5):
+if sys.hexversion < 0x03050000:
     raise UnsupportedVersionException("Your version of Python is unsupported. "
                                       "Pyshark requires Python >= 3.5 & Wireshark >= 2.2.0. "
                                       " Please upgrade or use pyshark-legacy, or pyshark version 0.3.8")
+
 
 from pyshark.capture.live_capture import LiveCapture
 from pyshark.capture.live_ring_capture import LiveRingCapture

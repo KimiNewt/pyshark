2e768d4c147465fb31be13d20062d2dc36d0ca21
bnlrnz <lorenzb@tu-freiberg.de>
2022-01-05 11:55:05 +0100

    added packaging to install requirements

--- src/setup.py
+++ src/setup.py
@@ -9,7 +9,7 @@ setup(
     version="0.4.3",
     packages=find_packages(),
     package_data={'': ['*.ini', '*.pcapng']},
-    install_requires=['lxml', 'py'],
+    install_requires=['lxml', 'py', 'packaging'],
     tests_require=['pytest'],
     url="https://github.com/KimiNewt/pyshark",
     license="MIT",

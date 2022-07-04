b06c358afdc868b066879366155222b1e95c2ccb
MiaoTony <41962043+miaotony@users.noreply.github.com>
2022-07-05 03:43:04 +0800

    Get `config.ini` from the current directory first.

--- src/pyshark/config.py
+++ src/pyshark/config.py
@@ -4,10 +4,19 @@ from configparser import ConfigParser
 
 import pyshark
 
-CONFIG_PATH = os.path.join(os.path.dirname(pyshark.__file__), 'config.ini')
+
+fp_config_path = os.path.join(os.getcwd(), 'config.ini')  # get config from the current directory
+pyshark_config_path = os.path.join(os.path.dirname(pyshark.__file__), 'config.ini')
 
 
 def get_config():
+    if os.path.exists(fp_config_path):
+        CONFIG_PATH = fp_config_path
+    elif os.path.exists(pyshark_config_path):
+        CONFIG_PATH = pyshark_config_path
+    else:
+        return None
+        
     config = ConfigParser()
     config.read(CONFIG_PATH)
     return config

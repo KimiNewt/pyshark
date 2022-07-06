920b602885470eb9f19dc535711662131c6c5541
MiaoTony <41962043+miaotony@users.noreply.github.com>
2022-07-06 16:38:45 +0800

    Change `CONFIG_PATH` to `config_path`.
    Use lowercase since it is not a constant any more.

--- src/pyshark/config.py
+++ src/pyshark/config.py
@@ -11,12 +11,12 @@ pyshark_config_path = os.path.join(os.path.dirname(pyshark.__file__), 'config.in
 
 def get_config():
     if os.path.exists(fp_config_path):
-        CONFIG_PATH = fp_config_path
+        config_path = fp_config_path
     elif os.path.exists(pyshark_config_path):
-        CONFIG_PATH = pyshark_config_path
+        config_path = pyshark_config_path
     else:
         return None
         
     config = ConfigParser()
-    config.read(CONFIG_PATH)
+    config.read(config_path)
     return config

d01bd7bd3669f9e5f1776128765ee18caf672fd3
MiaoTony <41962043+miaotony@users.noreply.github.com>
2022-07-06 17:14:14 +0800

    Use `pathlib` to replace `os.path` in `config.py`.

--- src/pyshark/config.py
+++ src/pyshark/config.py
@@ -1,18 +1,18 @@
-import os
+from pathlib import Path
 
 from configparser import ConfigParser
 
 import pyshark
 
 
-fp_config_path = os.path.join(os.getcwd(), 'config.ini')  # get config from the current directory
-pyshark_config_path = os.path.join(os.path.dirname(pyshark.__file__), 'config.ini')
+fp_config_path = Path.cwd() / 'config.ini'  # get config from the current directory
+pyshark_config_path = Path(pyshark.__file__).parent / 'config.ini'
 
 
 def get_config():
-    if os.path.exists(fp_config_path):
+    if Path.exists(fp_config_path):
         config_path = fp_config_path
-    elif os.path.exists(pyshark_config_path):
+    elif Path.exists(pyshark_config_path):
         config_path = pyshark_config_path
     else:
         return None

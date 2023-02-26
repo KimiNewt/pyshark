21d1ef8e5528a58d491b00c6be6a75d1041c019f
nser77 <104022475+nser77@users.noreply.github.com>
2023-02-26 10:42:40 +0100

    Update live_capture.py

--- src/pyshark/capture/live_capture.py
+++ src/pyshark/capture/live_capture.py
@@ -19,7 +19,7 @@ class LiveCapture(Capture):
     def __init__(self, interface=None, bpf_filter=None, display_filter=None, only_summaries=False,
                  decryption_key=None, encryption_type='wpa-pwk', output_file=None, decode_as=None,
                  disable_protocol=None, tshark_path=None, override_prefs=None, capture_filter=None,
-                 monitor_mode=False, use_json=False, use_ek=False,
+                 monitor_mode=False, use_json=False, use_ek=False, use_redis=False, redis_key=None,
                  include_raw=False, eventloop=None, custom_parameters=None,
                  debug=False):
         """Creates a new live capturer on a given interface. Does not start the actual capture itself.

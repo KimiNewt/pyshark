96e066ef4fd32c00e5401a50ead7c72443387574
nser77 <104022475+nser77@users.noreply.github.com>
2023-02-26 12:45:48 +0100

    Update tshark_redis.py

--- src/pyshark/tshark/output_parser/tshark_redis.py
+++ src/pyshark/tshark/output_parser/tshark_redis.py
@@ -18,14 +18,16 @@ except ImportError:
 
 class TsharkRedisParser(TsharkJsonParser):
     
-    def __init__(self, tshark_version=None):
+    def __init__(self, tshark_version=None, redis_host=None, redis_key=None):
         super().__init__()
         self._tshark_version = tshark_version
+        self.redis_host=redis_host
+        self.redis_key=redis_key
 
     def _parse_single_packet(self, packet):
-        return json_packet_to_redis(packet)
+        return json_packet_to_redis(packet, self.redis_host, self.redis_key)
 
-def json_packet_to_redis(json_pkt):
-    r=redis.Redis(host="redis.it.home.local", port=6379, db=0)
-    r.lpush("logstash", json_pkt)
+def json_packet_to_redis(json_pkt, redis_host, redis_key):
+    r=redis.Redis(host=redis_host, port=6379, db=0)
+    r.lpush(redis_key, json_pkt)
     return True
